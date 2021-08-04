845ef565cc63cc4aede837aec35e83652c5c0747
lathomasexport <laurent.thomas@cern.ch>
2021-08-04 22:41:32 +0200

    Cosmetic changes

--- PhysicsTools/NanoAOD/python/nano_cff.py
+++ PhysicsTools/NanoAOD/python/nano_cff.py
@@ -159,23 +159,6 @@ from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMet
 
 def nanoAOD_recalibrateMETs(process,isData):
 
-    '''
-    nanoAOD_Puppi_switch = cms.PSet(
-        nanoAOD_MakePuppies_switch = cms.untracked.bool(False)
-    )
-    for modifier in run2_miniAOD_80XLegacy,run2_nanoAOD_94X2016,run2_nanoAOD_94XMiniAODv1,run2_nanoAOD_94XMiniAODv2,run2_nanoAOD_102Xv1,run2_nanoAOD_106Xv1:
-        modifier.toModify(nanoAOD_Puppi_switch, nanoAOD_MakePuppies_switch =  cms.untracked.bool(True))
-        
-
-    if nanoAOD_Puppi_switch.nanoAOD_MakePuppies_switch:
-        from PhysicsTools.PatAlgos.slimming.puppiForMET_cff import makePuppiesFromMiniAOD
-        makePuppiesFromMiniAOD(process,True)
-        process.puppiNoLep.useExistingWeights = True
-        process.puppi.useExistingWeights = True
-        run2_nanoAOD_106Xv1.toModify(process.puppiNoLep, useExistingWeights = False)
-        run2_nanoAOD_106Xv1.toModify(process.puppi, useExistingWeights = False)
-        print("will make Puppies on top of MINIAOD")
-    '''
     
     from PhysicsTools.PatAlgos.slimming.puppiForMET_cff import makePuppiesFromMiniAOD
     makePuppiesFromMiniAOD(process,True)
@@ -185,8 +168,6 @@ def nanoAOD_recalibrateMETs(process,isData):
     run2_nanoAOD_106Xv1.toModify(process.puppi, useExistingWeights = False)
     print("will make Puppies on top of MINIAOD")
     
-    #end Laurent
-
     # add DeepMETs
     nanoAOD_DeepMET_switch = cms.PSet(
         nanoAOD_addDeepMET_switch = cms.untracked.bool(True), # decide if DeeMET should be included in Nano
@@ -254,7 +235,7 @@ def nanoAOD_recalibrateMETs(process,isData):
                             getJetMCFlavour= False
         )
         print("nanoAOD_PuppiV15_switch.reclusterJets is true")
-        #Laurent
+
     
     runMetCorAndUncFromMiniAOD(process,isData=isData,metType="Puppi",postfix="Puppi",jetFlavor="AK4PFPuppi", recoMetFromPFCs=bool(nanoAOD_PuppiV15_switch.recoMetFromPFCs), reclusterJets=bool(nanoAOD_PuppiV15_switch.reclusterJets))
     process.nanoSequenceCommon.insert(2,cms.Sequence(process.puppiMETSequence+process.fullPatMetSequencePuppi))
